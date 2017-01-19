448648f04d2f15b9e2fc3497ace44fc63714782d
Goryudyuma <umaretekyoumade@gmail.com>
2017-01-19 19:47:46 +0900

    こんな感じでデータベースに聞きに行くのはどうなんだろうか。 とりあえずやってみようと思う。何処かで無理そうだったら戻ってこよう。

--- lib/Database/database.go
+++ lib/Database/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/Goryudyuma/tlm/lib/User"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -14,4 +15,34 @@ func (db database) New(username, password, dbname string) error {
 	}
 	db.Client = dbclient
 	return nil
+}
+
+type CheckLoginType struct {
+	UserID user.UserID
+	Exit   chan string
+	Err    chan error
+}
+
+func (db database) CheckLogin(u <-chan CheckLoginType, exit <-chan bool) {
+	stmt, err := db.Client.Prepare("SELECT token FROM user WHERE id = ? AND owner = 1 AND lastlogin > NOW()")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+	for {
+		select {
+		case CheckLogin := <-u:
+			{
+				var ret string
+				if err := stmt.QueryRow(CheckLogin.UserID).Scan(&ret); err != nil {
+					CheckLogin.Err <- err
+				}
+				CheckLogin.Exit <- ret
+			}
+		case <-exit:
+			{
+				break
+			}
+		}
+	}
 }
